1620413eb8b4fa8c6f8767fbc1d4f75c9f5f98d2
princi singhal <princisinghal64@gmail.com>
2026-09-17 00:28:35 +0530

    add files

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./Header"
+
+function App() {
+
+  return (
+    <div>
+      <Header/>
+      <h1>memory game</h1>
+      <h2>find matching pairs</h2>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Header.jsx
@@ -0,0 +1,8 @@
+function Header(){
+    return(
+        <div>
+            <h4>boost your memory</h4>
+        </div>
+    )
+}
+export default Header
